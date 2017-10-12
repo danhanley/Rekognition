1da620634a487e4e40943eb9905d3d5048850eca
Dan Hanley <hanley.dan@gmail.com>
2017-10-12 13:33:21 +0100

    basic dir walk

--- src/combiner/combiner.py
+++ src/combiner/combiner.py
@@ -1,13 +1,36 @@
 import os
+import json
 
 DATA_FOLDER = "../../data"
 
 def main():
-    print "Hello World!!"
+    print "Establishing dimensionality of this universe"
     for dirname, dirnames, filenames in os.walk(DATA_FOLDER):
-        print dirname
+        print "Processing files in: " + dirname
         for filename in filenames:
-            print(os.path.join(dirname, filename))
+            print(filename)
+            content = readfile(dirname, filename)
+            obj = parseJSON(content, filename)
+            if obj is not None:
+                print obj
+
+
+
+def parseJSON(content, filename):
+    try:
+        json_obj = json.loads(content)
+    except:
+        print "ERROR: " + filename + " is not a happy json file"
+    else:
+        return json_obj["labels"]
+
+
+def readfile(dirname, filename):
+    file = open(os.path.join(dirname, filename), "r")
+    content = file.read()
+    file.close()
+    return content
+
 
 if __name__== "__main__":
     main()
